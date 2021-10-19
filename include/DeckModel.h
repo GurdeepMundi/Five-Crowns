8536cfd34f50f997b9d7585ca41f9d1a515d9206
Muhammad Saleh <msalehlm@hotmail.com>
2021-10-19 17:22:39 -0600

    something

--- include/DeckModel.h
+++ include/DeckModel.h
@@ -1,7 +1,7 @@
 /*
 * CPSC 3720 Fall 2021
 * Sahil Devnani, Gurdeep Singh, Muhammad Saleh <sahil.devnani@Uleth.ca>
-* File:DeckModel.h
+* File: DeckModel.h
 * Date Created: 16/10/2021
 */
 #ifndef DECKMODEL_H
