0d9416ebe7d1ff3c6e0332d0b228877fbbfdb8a8
Muhammad Saleh <msalehlm@hotmail.com>
2021-10-19 17:26:38 -0600

    CardModel.cpp Fix

--- src/CardModel.cpp
+++ src/CardModel.cpp
@@ -4,7 +4,7 @@
 * File: CardModel.cpp
 * Date Created: 16/10/2021
 */
-#include "./include/CardModel.h"
+#include "../include/CardModel.h"
 
 void Card::setScoreValue(int scoreValue) {
   this->scoreValue = scoreValue;
