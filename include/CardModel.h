30caeeb1058369097cf98f653a4d1504785d72f2
Gurdeep Singh <gurdeepmundi10@gmail.com>
2021-10-21 16:35:20 -0600

    some documentation

--- include/CardModel.h
+++ include/CardModel.h
@@ -14,10 +14,17 @@ enum Suits {CLUBS, HEARTS, SPADES, DIAMONDS, STARS};
 class CardModel {
  public:
   // Sahil is working on this
+  /**Used to assign a score value to card
+  *@param scoreValue score value of the card
+  */
   void setScoreValue(int scoreValue);
   // Sahil is working on this
+  /**Used to get score value of the card
+  */
   int getScoreValue();
-
+  /**Used to set the rank (number)
+  *@param 
+  */
   void setRank(int r);
   // Muhammad
   void setSuit(Suits s);
