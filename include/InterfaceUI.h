f97b7616e9f7b50951ef51d8fdb93b3e033c7f98
sahil devnani <sahil.devnani@uleth.ca>
2021-10-21 19:49:51 -0600

    Interface Ui comments

--- include/InterfaceUI.h
+++ include/InterfaceUI.h
@@ -13,11 +13,29 @@
 
 class InterfaceUI {
  public:
+   /** This function asks for the user name.
+   * @ param string is used as an input for the user name.
+   */
    std:: string askForUserName();
+   /**
+   * This function displays a welcoe Message to the user.
+   */
    void WelcomeMessage();
+   /**
+   * This function asks user for their choice when playing the game.
+   */
    void askChoice();
+   /**
+   * This function displays the game menu to the user.
+   */
    void displayMenu();
+   /**
+   * This function displays the state of the game to the user.
+   */
    void displayState();
+   /**
+   * This function displays a goodbye message to the user.
+   */
    void byeMessage();
 };
 #endif
